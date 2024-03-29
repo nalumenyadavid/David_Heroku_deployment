de7cd7322df15ce4bb0958d0d93c16098b63b4cc
nalumenyadavid <59600752+nalumenyadavid@users.noreply.github.com>
2024-03-29 13:21:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def predict():
             return render_template('index.html', prediction_text="An error occurred. Please check your input.")
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True)
