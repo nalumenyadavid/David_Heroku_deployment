22460ac188b753d28b46acadd46bdc3f9ae709a7
nalumenyadavid <59600752+nalumenyadavid@users.noreply.github.com>
2024-03-31 10:49:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,4 @@ def predict():
       return render_template('index.html', prediction_text="An unexpected error occurred. Please try again later.")
 
 if __name__ == '__main__':
- 
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port=port) 
+  app.run(debug=True)
