562768b691407567c4a5caf37e07fe1073084479
nalumenyadavid <59600752+nalumenyadavid@users.noreply.github.com>
2024-04-02 14:46:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -48,5 +48,5 @@ def predict():
       print(f"Error during prediction: {e}")
       return render_template('index.html', prediction_text="An unexpected error occurred. Please try again later.")
 
-if __name__ == '__main__':
-  app.run(debug=True)
+#if __name__ == '__main__':
+#  app.run(debug=True)
